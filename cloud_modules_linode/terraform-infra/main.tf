3ed31f6491b5eff2623d42456387a17ad4e966a9
Marek Zupnik <mzupnik@akamai.com>
2025-10-06 11:25:45 +0200

    added local_ip

--- cloud_modules_linode/terraform-infra/main.tf
+++ cloud_modules_linode/terraform-infra/main.tf
@@ -8,6 +8,17 @@ variable "tenant_id" {
   type        = string
 }
 
+variable "local_ip" {
+  description = "Local IP address for the firewall allow list"
+  type        = string
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key for the bastion host"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}
+
 variable "region" {
   description = "Region for all resources"
   type        = string
@@ -34,7 +45,7 @@ resource "linode_object_storage_bucket" "input_data_storage" {
 
 resource "linode_object_storage_key" "input_data_storage_key" {
   depends_on = [linode_object_storage_bucket.input_data_storage]
-  label = "input-data-storage-key-${var.tenant_id}"
+  label      = "input-data-storage-key-${var.tenant_id}"
   bucket_access {
     region      = linode_object_storage_bucket.input_data_storage.region
     bucket_name = linode_object_storage_bucket.input_data_storage.label
@@ -49,7 +60,7 @@ resource "linode_object_storage_bucket" "internal_result_storage" {
 
 resource "linode_object_storage_key" "internal_result_storage_key" {
   depends_on = [linode_object_storage_bucket.internal_result_storage]
-  label = "internal-result-storage-key-${var.tenant_id}"
+  label      = "internal-result-storage-key-${var.tenant_id}"
   bucket_access {
     region      = linode_object_storage_bucket.internal_result_storage.region
     bucket_name = linode_object_storage_bucket.internal_result_storage.label
@@ -63,20 +74,20 @@ resource "linode_lke_cluster" "datastream_lke_cluster" {
   k8s_version = "1.33"
 
   pool {
-    type  = "g6-nanode-1"
-    count = 3
+    type     = "g6-standard-1"
+    count    = 1 # TODO 3
   }
 }
 
 resource "local_file" "kubeconfig" {
   depends_on = [linode_lke_cluster.datastream_lke_cluster]
-  filename = "${path.module}/kubeconfig-${var.tenant_id}.yaml"
-  content = base64decode(linode_lke_cluster.datastream_lke_cluster.kubeconfig)
+  filename   = "${path.module}/kubeconfig-${var.tenant_id}.yaml"
+  content    = base64decode(linode_lke_cluster.datastream_lke_cluster.kubeconfig)
 }
 
 resource "linode_vpc" "datastream_vpc" {
-  label  = "datastream-vpc-${var.tenant_id}"
-  region = var.region
+  label       = "datastream-vpc-${var.tenant_id}"
+  region      = var.region
   description = "Datastream VPC for tenant ${var.tenant_id}"
 }
 
@@ -89,11 +100,56 @@ resource "linode_vpc_subnet" "datastream_subnet" {
 resource "linode_database_postgresql_v2" "postgresql_instance" {
   depends_on = [
   ]
-  label     = "datastream-postgresql-${var.tenant_id}"
-  engine_id = "postgresql/14"
-  region    = var.region
-  type      = "g6-nanode-1"
+  label      = "datastream-postgresql-${var.tenant_id}"
+  engine_id  = "postgresql/14"
+  region     = var.region
+  type       = "g6-nanode-1"
   allow_list = [linode_vpc_subnet.datastream_subnet.ipv4]
+}
+
+resource "linode_instance" "bastion" {
+  label           = "bastion-${var.tenant_id}"
+  region          = var.region
+  type            = "g6-nanode-1"
+  image           = "linode/ubuntu22.04"
+  authorized_keys = [trimspace(file(var.ssh_public_key))]
+  private_ip      = true
+  interface {
+    purpose = "public"
+  }
+  interface {
+    purpose   = "vpc"
+    subnet_id = linode_vpc_subnet.datastream_subnet.id
+  }
+}
+
+resource "linode_firewall" "datastream_firewall" {
+  label           = "bastion-and-k8s-fw-${var.tenant_id}"
+  inbound_policy  = "DROP"
+  outbound_policy = "DROP"
+  inbound {
+    label    = "allow_tcp_22_from_local_ip"
+    action   = "ACCEPT"
+    protocol = "TCP"
+    ports    = "22"
+    ipv4     = [format("%s/32", var.local_ip)]
+  }
+  inbound {
+    label    = "allow_tcp_6443_from_bastion_ip"
+    action   = "ACCEPT"
+    protocol = "TCP"
+    ports    = "6443"
+    ipv4     = [format("%s/32", tolist(linode_instance.bastion.ipv4)[1])]
+  }
+  outbound {
+    label    = "allow_all_outbound_traffic"
+    action   = "ACCEPT"
+    protocol = "TCP"
+    ports    = "1-65535"
+    ipv4     = ["0.0.0.0/0"]
+    ipv6     = ["::/0"]
+  }
+  linodes = [linode_instance.bastion.id]
 }
 
 output "tenant_id" {
@@ -128,4 +184,8 @@ output "postgresql_instance" {
     root_password = linode_database_postgresql_v2.postgresql_instance.root_password
   }
   sensitive = true
+}
+
+output "bastion_public_ip" {
+  value = tolist(linode_instance.bastion.ipv4)[0]
 }
